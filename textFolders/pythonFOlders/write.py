d7ddbc9738ad9421783992d66626a08a06ebad1b
jayawickrama <nishantharaka2004@gmail.com>
2024-12-10 09:46:28 +0530

    append the new lone to the list

--- textFolders/pythonFOlders/write.py
+++ textFolders/pythonFOlders/write.py
@@ -1,2 +1,4 @@
 with open("textFolders/myFile1",'w')as file:
-    file.writelines(["helow world \nhello python"])
+    list =["hello python \nthis is out python class"]  #sending values to the text file as a list type
+    list.append("\n appent the text using append ")    # append the new line to the text file
+    file.writelines(list)                              #write the list type value in the text file
