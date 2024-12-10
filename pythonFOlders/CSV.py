538004221a8fe674cb404864bde130eb5c4bf608
jayawickrama <nishantharaka2004@gmail.com>
2024-12-10 14:52:05 +0530

    creating csv

--- pythonFOlders/CSV.py
+++ pythonFOlders/CSV.py
@@ -2,5 +2,8 @@ import csv
 
 csv_file_path ="Json/customers-100.csv"
 with open(csv_file_path,newline="")as csv_file:
-    csv=csv.reader(csv_file)
+    csv=csv.reader(csv_file)            #csv.reader by uing this we can read the csv files 
     print(csv,type(csv))
+
+
+  
