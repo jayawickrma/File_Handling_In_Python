76a0a1de5c65c3ca9b92392d0d8ff3dbf1351386
jayawickrama <nishantharaka2004@gmail.com>
2024-12-10 09:52:52 +0530

    using 'a' mode append the data to the textfile

--- textFolders/pythonFOlders/write.py
+++ textFolders/pythonFOlders/write.py
@@ -1,4 +1,7 @@
 with open("textFolders/myFile1",'w')as file:
     list =["hello python \nthis is out python class"]  #sending values to the text file as a list type
-    list.append("\n appent the text using append ")    # append the new line to the text file
+    # list.append("\nappend the text using append ")    # append the new line to the text file
     file.writelines(list)                              #write the list type value in the text file
+
+with open("textFolders/myFile1",'a')as file:        # 'a' this is the append mode in the file handling
+    file.write("\noiiiiiiiiiiiiiiiiiii")
