d8905dce25493a4ebc1289e4893f101d4a3b65d0
jayawickrama <nishantharaka2004@gmail.com>
2024-12-10 12:35:05 +0530

    student

--- pythonFOlders/studentsDataTask.py
+++ pythonFOlders/studentsDataTask.py
@@ -6,19 +6,54 @@ import json
         #save the updated data back to the json file 
 
 
-name=input("Enter Your Name :")
-address = input("Enter your address :")
-age=input("enter your age :")
 
-studentData ={
-    "Name ": name,
-    "Address " : address,
-    "Age ": age
-}
-with open("pythonFOlders/textFOlders/studentsData",'w')as file:
-    student = json.dumps(studentData)
-    file.write(student)
+def read_students(file_name):
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+    return data
 
-with open("pythonFOlders/textFOlders/studentsData",'r')as file:
-    read_student =json.load(file)
-    print(read_student)
+def display_top_students(data):
+    print("Students who scored above 75:")
+    for student in data['students']:
+        if student['score'] > 75:
+            print(f"- {student['name']}")
+
+
+def add_new_student(data):
+
+    name = input("Enter the new student's name: ")
+    address =input("input your address")
+    score = int(input(f"Enter {name}'s score: "))
+    new_student = {"name": name,"address": address, "score": score}
+    data['students'].append(new_student)
+    print(f"Added {name} with a score of {score}.")
+    return data
+
+def save_data(file_name, data):
+    with open(file_name, 'w') as file:
+        json.dump(data, file, indent=4)
+    print(f"Data saved to {file_name} successfully.")
+
+    file_name="pythonFOlders/textFOlders/studentsData"
+
+
+while True:
+    print("\n----Score manager----")
+    print("1-View students")
+    print("2-Add student")
+    print("3-view top Scores students")
+    try:
+        press = int(input("Enter your choice: "))
+        if press == 1:
+            read_students()
+        elif press == 2:
+            add_new_student()
+        elif press == 3:
+           display_top_students()
+        elif press == 4:
+            print("end")
+            break
+        else:
+            print("Invalid choice. Please enter 1, 2, or 3.\n")
+    except ValueError:
+        print("Invalid input! Please enter a valid number.\n")
