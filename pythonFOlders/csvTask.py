8c4d9920496fe4f0b7d6d882fdf27e2a16e15386
jayawickrama <nishantharaka2004@gmail.com>
2024-12-10 16:41:57 +0530

    call to the function suing functioin

---
+++ pythonFOlders/csvTask.py
@@ -0,0 +1,9 @@
+# your task creating a pythhon program two manage the company employee recodes stording csv files.the programs should be the employee from the csv file.filter the recode basic on te condition and write the filters recode to hte csv fil.
+# input file emplooyee.csv containf the followinfg fields emplooyeId name , depadment ,salary
+
+# it should contains record of employees who's salaries are above 57000.the field should remain the same
+
+        #read the input file (use csv.reader to read employes.csv and display all the records on the console)
+        #filter the record identify employees with the salary >57000
+        #write the filtered record use csv.DictWritter() to write the filtered record to a new file name highSalaryEmployees.csv
+        
