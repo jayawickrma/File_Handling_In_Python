6353489eaf9584bc65047fd438484b07d84038e5
jayawickrama <nishantharaka2004@gmail.com>
2024-12-10 09:41:12 +0530

    write content using write line as list type

---
+++ textFolders/pythonFOlders/write.py
@@ -0,0 +1,2 @@
+with open("textFolders/myFile1",'w')as file:
+    file.writelines(["helow world \nhello python"])
--- write.py
+++
@@ -1,3 +0,0 @@
-with open("textFolders/myFile1",'w')as file:
-    file.write("Hello world.\n")
-    file.write("this class in the Write Mode")
