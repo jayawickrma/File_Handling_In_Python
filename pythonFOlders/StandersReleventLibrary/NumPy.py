adda70014287fe859137660b8408964f51d1c349
jayawickrama <nishantharaka2004@gmail.com>
2024-12-17 09:28:55 +0530

    using numPy

--- pythonFOlders/StandersReleventLibrary/NumPy.py
+++ pythonFOlders/StandersReleventLibrary/NumPy.py
@@ -1,4 +1,14 @@
 import NumPy as np;
 
-a=np.array([2,3,4])
-print(a,type(a))
+a =np.array([1,2,3,4])
+b=np.array([5,6,7,8])
+
+resul_Substraction =b-a
+addition=a+b
+devition =a/b
+multiple=a*b
+
+print(resul_Substraction)
+print(addition)
+print(devition)
+print(multiple)
